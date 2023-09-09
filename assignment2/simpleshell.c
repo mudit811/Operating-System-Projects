73ab46c84fd8f558bd8e664e1b81a2f261f66053
Mudit Bansal <119156022+mudit811@users.noreply.github.com>
2023-09-10 02:45:11 +0530

    update

--- assignment2/simpleshell.c
+++ assignment2/simpleshell.c
@@ -6,12 +6,15 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 
-char Input[100];
-char *Args[20];
+#define MAX_INPUT_SIZE 256
+#define MAX_HISTORY_SIZE 10
+#define MAx_ARGUMENT_SIZE 16
+
+char Input[MAX_INPUT_SIZE];
+char *Args[MAx_ARGUMENT_SIZE];
 bool Background_process = false;
-// int historyCount = 0;
-
-
+char *history_book[MAX_HISTORY_SIZE];
+int historycount=0;
 void parse_command()
 {
     char *token = strtok(Input, " ");
@@ -25,19 +28,18 @@ void parse_command()
     Args[i] = NULL;
 }
 
-
 int create_process_and_run(char *command)
 {
     pid_t status = fork();
     if (status < 0)
     {
-        printf("Something bad happened\n");
+        printf("Child not created\n");
         exit(0);
     }
     else if (status == 0)
     {
-        printf("I am the child (%d)\n", getpid());
-        parse_command();
+        // printf("I am the child (%d)\n", getpid());
+        // parse_command();
         execvp(Args[0], Args);
         perror("Error in executing command");
         exit(EXIT_FAILURE);
@@ -47,7 +49,7 @@ int create_process_and_run(char *command)
         // Background process
         if (!Background_process)
         {
-            wait(NULL);
+            waitpid(status, NULL, 0);
         }
     }
     else
@@ -64,31 +66,38 @@ int create_process_and_run(char *command)
             perror("Fork failed");
             exit(EXIT_FAILURE);
         }
-        printf("I am the parent Shell\n");
+        // printf("I am the parent Shell\n");
     }
-
 }
-
 
 int launch(char *command)
 {
     int status;
+    parse_command();
     status = create_process_and_run(command);
     return status;
 }
 
-
-char * read_user_input()
+char *read_user_input()
 {
 
-    fgets(Input, sizeof(Input), stdin);
+    if (fgets(Input, MAX_INPUT_SIZE, stdin) == NULL)
+    {
+        perror("Failed to get input");
+        exit(EXIT_FAILURE);
+    }
 
-    Input[strlen(Input) - 1] = '\0';
+    if (strlen(Input) > 0 && Input[strlen(Input) - 1] == '\n')
+    {
+        Input[strlen(Input) - 1] = '\0';
+    }
+
     if (Input[strlen(Input) - 1] == '&')
     {
         Background_process = true;
         Input[strlen(Input) - 1] = '\0'; // Remove the "&" symbol
     }
+
     else
     {
         Background_process = false;
@@ -103,7 +112,27 @@ void shell_loop()
     {
         printf("iiitd@possum:~$ ");
         char *command = read_user_input();
-        status = launch(command);
+
+        if (!strcmp("", command))
+            continue;
+
+        if (!strcmp("exit", command))
+            break;
+        if (!strcmp(command, "history"))
+        {
+            // Display history
+            for (int i = 0; i < historycount; i++)
+            {
+                printf("%d: %s\n", i + 1, history_book[i]);
+            }
+        }
+        else
+        {
+            // Store command in history
+            history_book[historycount] = strdup(command);
+            historycount++;
+            status = launch(command);
+        }
     } while (status);
 }
 
