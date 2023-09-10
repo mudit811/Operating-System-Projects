a358fae6495d4fa05713dca7f9642539919ddcf2
theniceguy8731 <kushagra22263@iiitd.ac.in>
2023-09-10 17:07:55 +0530

    error handling

--- assignment2/simpleshell.c
+++ assignment2/simpleshell.c
@@ -6,62 +6,64 @@
 #include <stdbool.h>
 #include <sys/wait.h>
 
-#define MAX_INPUT_SIZE 256
-#define MAX_HISTORY_SIZE 10
-#define MAx_ARGUMENT_SIZE 16
-
-char Input[MAX_INPUT_SIZE];
-char *Args[MAx_ARGUMENT_SIZE];
-bool Background_process = false;
-char *history_book[MAX_HISTORY_SIZE];
+char *history_book[20];
 int historycount=0;
-void parse_command()
-{
+
+int secure_strcmp(const char *str1, const char *str2) {
+    while (*str1 && *str2){
+        if (*str1 != *str2){return 1;}
+        str1++;
+        str2++;
+    }
+    return (*str1 == '\0' && *str2 == '\0') ? 0 : 1;
+}
+
+char Input[256];
+char *Args[16];
+bool back_proc = false;
+
+//breaks the input command into function and it's arguments by using strtok
+void parse_command(){
+    //The  strtok()  function  breaks a string into a sequence of zero or more nonempty tokens.
     char *token = strtok(Input, " ");
+    //error checking for strtok
+    if(token==NULL){
+        exit(EXIT_FAILURE);
+    }
+    //
     int i = 0;
-    while (token != NULL)
-    {
+    while (token != NULL){
         Args[i] = token;
         token = strtok(NULL, " ");
         i++;
     }
     Args[i] = NULL;
 }
-
-int create_process_and_run(char *command)
-{
+//the final function which will run the command
+int create_process_and_run(char *command){
     pid_t status = fork();
-    if (status < 0)
-    {
+    if (status <= -1){
         printf("Child not created\n");
         exit(0);
     }
-    else if (status == 0)
-    {
+    else if (status == 0){
         // printf("I am the child (%d)\n", getpid());
         // parse_command();
         execvp(Args[0], Args);
         perror("Error in executing command");
         exit(EXIT_FAILURE);
     }
-    else if (status > 0)
-    {
+    else if (status > 0){
         // Background process
-        if (!Background_process)
-        {
+        if (back_proc==0){
             waitpid(status, NULL, 0);
         }
     }
-    else
-    {
+    else{
         int ret;
         int pid = wait(&ret);
-        if (WIFEXITED(ret))
-        {
-            printf("% d Exit = % d\n", pid, WEXITSTATUS(ret));
-        }
-        else
-        {
+        if (WIFEXITED(ret)){printf("% d Exit = % d\n", pid, WEXITSTATUS(ret));}
+        else{
             printf("Abnormal termination of % d\n", pid);
             perror("Fork failed");
             exit(EXIT_FAILURE);
@@ -69,76 +71,63 @@ int create_process_and_run(char *command)
         // printf("I am the parent Shell\n");
     }
 }
-
-int launch(char *command)
-{
+//the function used to transition from taking input and processing it to running the command
+int launch(char *command){
     int status;
     parse_command();
     status = create_process_and_run(command);
     return status;
 }
-
-char *read_user_input()
-{
-
-    if (fgets(Input, MAX_INPUT_SIZE, stdin) == NULL)
-    {
+//used to take input of a function
+char *read_user_input(){
+    //error checking while taking input
+    if (fgets(Input, 256, stdin) == NULL){
         perror("Failed to get input");
         exit(EXIT_FAILURE);
     }
-
-    if (strlen(Input) > 0 && Input[strlen(Input) - 1] == '\n')
-    {
+    //first it removes the newline character
+    if (strlen(Input) > 0 && Input[strlen(Input) - 1] == '\n'){
         Input[strlen(Input) - 1] = '\0';
     }
-
-    if (Input[strlen(Input) - 1] == '&')
-    {
-        Background_process = true;
+    //then it check if there is an ampersand or not
+    if (Input[strlen(Input) - 1] == '&'){
+        back_proc = true;
         Input[strlen(Input) - 1] = '\0'; // Remove the "&" symbol
     }
-
-    else
-    {
-        Background_process = false;
+    //if there is no ampersand that means it is not a background process
+    else{
+        back_proc = false;
     }
     return Input;
 }
-
-void shell_loop()
-{
+//running shell infinite loop
+void shell_loop(){
     int status;
-    do
-    {
-        printf("iiitd@possum:~$ ");
+    do{
+        printf("$: ");
+        //command points to the value returned from read_user_input
         char *command = read_user_input();
-
-        if (!strcmp("", command))
-            continue;
-
-        if (!strcmp("exit", command))
-            break;
-        if (!strcmp(command, "history"))
-        {
-            // Display history
-            for (int i = 0; i < historycount; i++)
-            {
+        //check if command is empty
+        if (!secure_strcmp("", command)){continue;}
+        //check in command is exit
+        if (!secure_strcmp("exit", command)){break;}
+        //check if the command wants history
+        if (secure_strcmp(command, "history")==0){
+            for (int i = 0; i < historycount; i++){
                 printf("%d: %s\n", i + 1, history_book[i]);
             }
         }
-        else
-        {
-            // Store command in history
+        else{
+            //store command in history
             history_book[historycount] = strdup(command);
             historycount++;
             status = launch(command);
         }
     } while (status);
 }
-
-int main()
-{
-
+//this is the main running loop
+int main(){
     bool runInBackground = false;
+    //the main shell loop is called
     shell_loop();
 }
